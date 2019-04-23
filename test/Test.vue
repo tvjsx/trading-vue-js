879ccc95e3d2f4075514918de37dcf0a1d08856b
C451 <codeallright@protonmail.com>
2019-04-23 18:30:21 +0300

    Add Channel overlay to the list, add simple.vue test

--- test/Test.vue
+++ test/Test.vue
@@ -1,58 +1,16 @@
 <template>
-<trading-vue :data="chart" :width="this.width" :height="this.height"
-        :color-back="colors.colorBack"
-        :color-grid="colors.colorGrid"
-        :color-text="colors.colorText">
-</trading-vue>
+    <div id="test-container">
+        <simple></simple>
+    </div>
 </template>
 
 <script>
-import TradingVue from '../src/TradingVue.vue'
-import Data from '../data/data.json'
-import Utils from '../src/stuff/utils.js'
+import Simple from './tests/Simple.vue'
 
 export default {
     name: 'app',
     components: {
-        TradingVue
-    },
-    methods: {
-        onResize(event) {
-            this.width = window.innerWidth
-            this.height = window.innerHeight
-        }
-    },
-    mounted() {
-        window.addEventListener('resize', this.onResize)
-        setTimeout(() => {
-            // Async data setup
-            this.$set(this, 'chart', Data)
-        }, 0)
-    },
-    beforeDestroy() {
-        window.removeEventListener('resize', this.onResize)
-    },
-    data() {
-        return {
-            chart: {}, // Data will be here,
-            width: window.innerWidth,
-            height: window.innerHeight,
-            colors: {
-                colorBack: '#fff',
-                colorGrid: '#eee',
-                colorText: '#333',
-            }
-        };
+        Simple
     }
 };
 </script>
-
-<style>
-html,
-body {
-    background-color: #000;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-}
-</style>
