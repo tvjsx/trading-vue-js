e746732a9c03b3aa764ec2324de91bbc57e3b909
C451 <codeallright@protonmail.com>
2019-04-23 18:07:38 +0300

    Add test app

---
+++ test/Test.vue
@@ -0,0 +1,58 @@
+<template>
+<trading-vue :data="chart" :width="this.width" :height="this.height"
+        :color-back="colors.colorBack"
+        :color-grid="colors.colorGrid"
+        :color-text="colors.colorText">
+</trading-vue>
+</template>
+
+<script>
+import TradingVue from '../src/TradingVue.vue'
+import Data from '../data/data.json'
+import Utils from '../src/stuff/utils.js'
+
+export default {
+    name: 'app',
+    components: {
+        TradingVue
+    },
+    methods: {
+        onResize(event) {
+            this.width = window.innerWidth
+            this.height = window.innerHeight
+        }
+    },
+    mounted() {
+        window.addEventListener('resize', this.onResize)
+        setTimeout(() => {
+            // Async data setup
+            this.$set(this, 'chart', Data)
+        }, 0)
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize)
+    },
+    data() {
+        return {
+            chart: {}, // Data will be here,
+            width: window.innerWidth,
+            height: window.innerHeight,
+            colors: {
+                colorBack: '#fff',
+                colorGrid: '#eee',
+                colorText: '#333',
+            }
+        };
+    }
+};
+</script>
+
+<style>
+html,
+body {
+    background-color: #000;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+}
+</style>
---
+++ test/index.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+
+import App from './Test.vue';
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+});
