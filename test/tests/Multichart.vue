e1b199bf205639bc14671556a3b214300948e8c8
C451 <codeallright@protonmail.com>
2019-05-09 17:25:26 +0300

    Add Multichart.vue test

---
+++ test/tests/Multichart.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="multi-chart">
+        <chartbox v-for="(timeframe, id) in charts" :key="id"
+            :tf="id" v-bind:data="timeframe"
+            :width="cbox_width" :height="cbox_height"
+            >
+        </chartbox>
+    </div>
+</template>
+
+<script>
+import Chartbox from './Multichart/Chartbox.vue'
+import Data from '../data/data_tf.json'
+import Utils from '../../src/stuff/utils.js'
+
+export default {
+    name: 'Multichart',
+    description: 'All charts must work correctly and independently',
+    components: {
+        Chartbox
+    },
+    methods: {
+        onResize(event) {
+            this.width = window.innerWidth
+            this.height = window.innerHeight - 50
+        }
+    },
+    computed: {
+        cbox_width() { return this.width / 2 - 1 },
+        cbox_height() { return this.height / 2 - 1 }
+    },
+    mounted() {
+        window.addEventListener('resize', this.onResize)
+        setTimeout(() => {
+            // Async data setup
+            this.$set(this, 'charts', Data)
+        }, 0)
+        this.onResize()
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize)
+    },
+    data() {
+        return {
+            charts: {}, // Data will be here,
+            width: window.innerWidth,
+            height: window.innerHeight
+        }
+    }
+};
+</script>
+
+<style>
+.multi-chart {
+    display: grid;
+    grid-template-columns: 50% 50%;
+    height: 100%;
+}
+</style>
---
+++ test/tests/Multichart/Chartbox.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="chart-box">
+    <trading-vue :data="chart" :titleTxt = "title"
+            :width="width" :height="height"
+            :color-title="colors.colorTitle"
+            :color-back="colors.colorBack"
+            :color-grid="colors.colorGrid"
+            :color-text="colors.colorText">
+    </trading-vue>
+</div>
+</template>
+
+<script>
+import TradingVue from '../../../src/TradingVue.vue'
+
+export default {
+    name: 'Chartbox',
+    props: ['tf', 'data', 'width', 'height'],
+    components: {
+        TradingVue
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    },
+    computed: {
+        chart() {
+            return { ohlcv: this.$props.data }
+        },
+        title() {
+            return 'Bitcoin ' + this.$props.tf
+        }
+    },
+    data() {
+        return {
+            colors: {
+                colorBack: '#fff',
+                colorGrid: '#eee',
+                colorText: '#333',
+                colorTitle: '#333'
+            }
+        };
+    }
+};
+</script>
+
+<style>
+.chart-box {
+    border: 1px solid;
+}
+</style>
