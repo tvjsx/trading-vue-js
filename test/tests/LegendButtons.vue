335c96795d682b35dc828d4acfeb3d7d7f78238e
C451 <codeallright@protonmail.com>
2019-05-19 20:35:32 +0300

    Add LegendButtons test & data

---
+++ test/tests/LegendButtons.vue
@@ -0,0 +1,54 @@
+<template>
+<trading-vue :data="chart" :width="this.width" :height="this.height"
+        :color-back="colors.colorBack"
+        :color-grid="colors.colorGrid"
+        :color-text="colors.colorText">
+</trading-vue>
+</template>
+
+<script>
+import TradingVue from '../../src/TradingVue.vue'
+import Data from '../data/data_buttons.json'
+import Utils from '../../src/stuff/utils.js'
+
+export default {
+    name: 'LegendButtons',
+    description: 'Legend buttons test',
+    components: {
+        TradingVue
+    },
+    methods: {
+        onResize(event) {
+            this.width = window.innerWidth
+            this.height = window.innerHeight - 50
+        }
+    },
+    mounted() {
+        window.addEventListener('resize', this.onResize)
+        setTimeout(() => {
+            // Async data setup
+            this.$set(this, 'chart', Data)
+        }, 0)
+        this.onResize()
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize)
+    },
+    data() {
+        return {
+            chart: {}, // Data will be here,
+            width: window.innerWidth,
+            height: window.innerHeight,
+            colors: {
+                colorBack: '#fff',
+                colorGrid: '#eee',
+                colorText: '#333',
+            }
+        };
+    }
+};
+</script>
+
+<style>
+
+</style>
