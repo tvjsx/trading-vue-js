f329c645bfed001fd8e4e0a786d5b6048d9885c4
C451 <codeallright@protonmail.com>
2019-05-20 14:28:42 +0300

    Add base64 icons & encoder tool

---
+++ src/components/LegendButton.vue
@@ -0,0 +1,10 @@
+<template>
+</template>
+<script>
+export default {
+    name: 'LegendButton',
+    props: ['id', 'icons']
+}
+</script>
+<style>
+</style>
---
+++ tools/base64_icons.js
@@ -0,0 +1,25 @@
+var fs = require('fs')
+
+const PATH = './src/icons/'
+
+function base64_encode(file) {
+    var bitmap = fs.readFileSync(file)
+    return new Buffer(bitmap).toString('base64')
+}
+
+let files = fs.readdirSync(PATH)
+let json = {}
+
+for (const file of files) {
+
+    if (file === '.DS_Store') continue
+
+    let b64 = base64_encode(PATH + file)
+    json[file] = b64
+
+    console.log(file)
+
+}
+
+let txt = JSON.stringify(json, null, 4)
+fs.writeFileSync('./src/stuff/icons.json', txt)
