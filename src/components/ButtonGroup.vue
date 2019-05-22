a564d8f3b83a2ade2b3750fcc432fb5ad8762701
C451 <codeallright@protonmail.com>
2019-05-22 14:02:48 +0300

    Legend buttons: event emitter

--- src/components/ButtonGroup.vue
+++ src/components/ButtonGroup.vue
@@ -2,7 +2,11 @@
 <span class="t-vue-lbtn-grp">
     <legend-button
         v-for="(b, i) in buttons" :key="i"
-        :id="b" :uuid="`${tv_id}-btn-g${grid_id}-${ov_id}`"
+        :id="b"
+        :tv_id="tv_id"
+        :ov_id="ov_id"
+        :grid_id="grid_id"
+        :index="index"
     >
     </legend-button>
 </span>
@@ -13,7 +17,7 @@ import LegendButton from './LegendButton.vue'
 
 export default {
     name: 'ButtonGroup',
-    props: ['buttons', 'ov_id', 'grid_id', 'tv_id'],
+    props: ['buttons', 'tv_id', 'ov_id', 'grid_id', 'index'],
     components: {
         LegendButton
     },
@@ -23,6 +27,6 @@ export default {
 </script>
 <style>
 .t-vue-lbtn-grp {
-    margin-left: 0.0em;
+    margin-left: 0.5em;
 }
 </style>
