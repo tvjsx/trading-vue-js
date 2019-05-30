a7968cb2e9e4b40eeee3acd741f477339d651301
C451 <codeallright@protonmail.com>
2019-05-30 15:14:50 +0300

    Add ESLint Vue.js plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:vue/essential"
+        "plugin:vue/recommended"
     ],
     "globals": {
         "Atomics": "readonly",
