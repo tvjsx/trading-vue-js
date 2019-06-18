76d0dfc50485b56135777aca74dffdcbe75be830
C451 <codeallright@protonmail.com>
2019-06-18 14:16:36 +0300

    Update library exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import TradingVue from './TradingVue.vue';
-import Overlay from './mixins/overlay.js';
-import Utils from './stuff/utils.js';
-import Constants from './stuff/constants.js';
+import TradingVue from './TradingVue.vue'
+import Overlay from './mixins/overlay.js'
+import Utils from './stuff/utils.js'
+import Constants from './stuff/constants.js'
+import Candle from './components/js/candle_ext.js'
+import Volbar from './components/js/volbar_ext.js'
+import LayoutCNV from './components/js/layout_cnv.js'
+
 
 TradingVue.install = function (Vue) {
     Vue.component(TradingVue.name, TradingVue)
@@ -10,12 +14,14 @@ TradingVue.install = function (Vue) {
 if (typeof window !== 'undefined' && window.Vue) {
     window.Vue.use(TradingVue)
     window.TradingVueLib = {
-        TradingVue, Overlay, Utils, Constants
+        TradingVue, Overlay, Utils, Constants,
+        Candle, Volbar, LayoutCNV
     }
 }
 
 export default TradingVue
 
 export {
-    TradingVue, Overlay, Utils, Constants
+    TradingVue, Overlay, Utils, Constants,
+    Candle, Volbar, LayoutCNV
 }
