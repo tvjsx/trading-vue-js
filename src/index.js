8acf0c3ccba810bd0f51589443832097c5864a2f
C451 <codeallright@protonmail.com>
2019-06-18 14:21:55 +0300

    Fix library exports

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Utils from './stuff/utils.js'
 import Constants from './stuff/constants.js'
 import Candle from './components/js/candle_ext.js'
 import Volbar from './components/js/volbar_ext.js'
-import LayoutCNV from './components/js/layout_cnv.js'
+import { layout_cnv } from './components/js/layout_cnv.js'
 
 
 TradingVue.install = function (Vue) {
@@ -15,7 +15,7 @@ if (typeof window !== 'undefined' && window.Vue) {
     window.Vue.use(TradingVue)
     window.TradingVueLib = {
         TradingVue, Overlay, Utils, Constants,
-        Candle, Volbar, LayoutCNV
+        Candle, Volbar, layout_cnv
     }
 }
 
@@ -23,5 +23,5 @@ export default TradingVue
 
 export {
     TradingVue, Overlay, Utils, Constants,
-    Candle, Volbar, LayoutCNV
+    Candle, Volbar, layout_cnv
 }
