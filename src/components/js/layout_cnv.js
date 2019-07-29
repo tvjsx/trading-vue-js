3b92a56e07e6505f813f63e5ea7b0124bc37cdb5
C451 <codeallright@protonmail.com>
2019-07-29 18:08:00 +0300

    Upgrade Candles & Volume overlays, fixes #41

--- src/components/js/layout_cnv.js
+++ src/components/js/layout_cnv.js
@@ -1,6 +1,8 @@
 // Claculates postions and sizes for candlestick
 // and volume bars for the given subset of data
 
+import Utils from '../../stuff/utils.js'
+
 export function layout_cnv(self) {
 
     let $p = self.$props
@@ -15,30 +17,39 @@ export function layout_cnv(self) {
     // the chart's height (VOLSCALE)
     let maxv = Math.max(...sub.map(x => x[5]))
     let vs =  $p.config.VOLSCALE * layout.height / maxv
-    var x1, x2, mid, prev = undefined
+    var x1, x2, w, avg_w, mid, prev = undefined
 
-    let splitter = layout.px_step > 5 ? 1 : 0
+    // Subset interval against main interval
+    let interval2 = Utils.detect_interval(sub)
+    let ratio = interval2 / $p.interval
+    let px_step2 = layout.px_step * ratio
+
+    let splitter = px_step2 > 5 ? 1 : 0
 
     // A & B are current chart tranformations:
     // A === scale,  B === Y-axis shift
     for (var i = 0; i < sub.length; i++) {
         let p = sub[i]
         mid = t2screen(p[0]) + 1
+
+        // Clear volume bar if there is a time gap
+        if (sub[i-1] && p[0] - sub[i-1][0] > interval2) {
+            prev = null
+        }
+
+        x1 = prev || Math.floor(mid - px_step2 * 0.5)
+        x2 = Math.floor(mid + px_step2 * 0.5) - 0.5
+
         candles.push({
             x: mid,
-            w: layout.px_step * $p.config.CANDLEW,
+            w: layout.px_step * $p.config.CANDLEW * ratio,
             o: p[1] * layout.A + layout.B,
             h: p[2] * layout.A + layout.B,
             l: p[3] * layout.A + layout.B,
             c: p[4] * layout.A + layout.B,
             raw: p
         })
-        // Clear volume bar if there is a time gap
-        if (sub[i-1] && p[0] - sub[i-1][0] > $p.interval) {
-            prev = null
-        }
-        x1 = prev || Math.floor(mid - layout.px_step * 0.5)
-        x2 = Math.floor(mid + layout.px_step * 0.5) - 0.5
+
         volume.push({
             x1: x1,
             x2: x2,
@@ -62,29 +73,44 @@ export function layout_vol(self) {
 
     let volume = []
 
-    let maxv = Math.max(...sub.map(x => x[1]))
+    // Detect data second dimention size:
+    let dim = sub[0] ? sub[0].length : 0
+
+    // Support special volume data (see API book), or OHLCV
+    // Data indices:
+    self._i1 = dim < 6 ? 1 : 5
+    self._i2 = dim < 6 ? (p => p[2]) : (p => p[4] > p[1])
+
+    let maxv = Math.max(...sub.map(x => x[self._i1]))
     let volscale = self.volscale || $p.config.VOLSCALE
     let vs = volscale * layout.height / maxv
     var x1, x2, mid, prev = undefined
 
-    let splitter = layout.px_step > 5 ? 1 : 0
+    // Subset interval against main interval
+    let interval2 = Utils.detect_interval(sub)
+    let ratio = interval2 / $p.interval
+    let px_step2 = layout.px_step * ratio
+
+    let splitter = px_step2 > 5 ? 1 : 0
 
     // A & B are current chart tranformations:
     // A === scale,  B === Y-axis shift
     for (var i = 0; i < sub.length; i++) {
         let p = sub[i]
         mid = t2screen(p[0]) + 1
+
         // Clear volume bar if there is a time gap
-        if (sub[i-1] && p[0] - sub[i-1][0] > $p.interval) {
+        if (sub[i-1] && p[0] - sub[i-1][0] > interval2) {
             prev = null
         }
-        x1 = prev || Math.floor(mid - layout.px_step * 0.5)
-        x2 = Math.floor(mid + layout.px_step * 0.5) - 0.5
+        x1 = prev || Math.floor(mid - px_step2 * 0.5)
+        x2 = Math.floor(mid + px_step2 * 0.5) - 0.5
+
         volume.push({
             x1: x1,
             x2: x2,
-            h: p[1] * vs,
-            green: p[2],
+            h: p[self._i1] * vs,
+            green: self._i2(p),
             raw: p
         })
         prev = x2 + splitter
