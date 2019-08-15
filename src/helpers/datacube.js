7f24b2e6744ee9bd716cb813af1bcb3f48781a8b
C451 <codeallright@protonmail.com>
2019-08-15 21:11:54 +0300

    DataCube: merge timeseries (unstable)

--- src/helpers/datacube.js
+++ src/helpers/datacube.js
@@ -146,24 +146,16 @@ export default class DataCube {
 
         for (var obj of objects) {
             if (Array.isArray(obj.v)) {
-
                 if (!Array.isArray(data)) continue
-
-                // If array is timeseries, merge it by timestamp
+                // If array is a timeseries, merge it by timestamp
                 // else merge by item index
-
-                // ...
-
+                if (obj.v[0] && obj.v[0].length >= 2) {
+                    this.merge_ts(obj, data)
+                } else {
+                    this.medge_objects(obj, data, [])
+                }
             } else if (typeof obj.v === 'object') {
-
-                // The only way to get Vue to update all stuff
-                // reactively is to create a brand new object.
-                // TODO: Is there a simpler approach?
-                let new_obj = {}
-                Object.assign(new_obj, obj.v)
-                Object.assign(new_obj, data)
-                this.Vue.$set(obj.p, obj.i, new_obj)
-
+                this.medge_objects(obj, data)
             }
         }
 
@@ -313,5 +305,134 @@ export default class DataCube {
             v: x
         }))
     }
+
+    merge_objects(obj, data, new_obj = {}) {
+
+        // The only way to get Vue to update all stuff
+        // reactively is to create a brand new object.
+        // TODO: Is there a simpler approach?
+        Object.assign(new_obj, obj.v)
+        Object.assign(new_obj, data)
+        this.Vue.$set(obj.p, obj.i, new_obj)
+
+    }
+
+    // Merge overlapping time series
+    merge_ts(obj, data) {
+
+        // Assume that both arrays are pre-sorted
+
+        if (!data.length) return obj.v
+
+        let r1 = [obj.v[0][0], obj.v[obj.v.length - 1][0]]
+        let r2 = [data[0][0],  data[data.length - 1][0]]
+
+        // Overlap
+        let o = [Math.max(r1[0],r2[0]), Math.min(r1[1],r2[1])]
+
+        if (o[1] >= o[0]) {
+
+            let { od, d1, d2 } = this.ts_overlap(obj.v, data, o)
+
+            obj.v.splice(...d1)
+            data.splice(...d2)
+
+            // Dst === Overlap === Src
+            if (!obj.v.length && !data.length) {
+                return od
+            }
+
+            // If src is totally contained in dst
+            if (!data.length) { data = obj.v.splice(d1[0]) }
+
+            // If dst is totally contained in src
+            if (!obj.v.length) { obj.v = data.splice(d2[0]) }
+
+
+            return this.combine(obj.v, od, data)
+
+        } else {
+
+            return this.combine(obj.v, [], data)
+
+        }
+
+    }
+
+    // TODO: review performance, move to worker
+    ts_overlap(arr1, arr2, range) {
+
+        const t1 = range[0]
+        const t2 = range[1]
+
+        let ts = {} // timestamp map
+
+        let a1 = arr1.filter(x => x[0] >= t1 && x[0] <= t2)
+        let a2 = arr2.filter(x => x[0] >= t1 && x[0] <= t2)
+
+        // Indices of segments
+        let id11 = arr1.indexOf(a1[0])
+        let id12 = arr1.indexOf(a1[a1.length - 1])
+        let id21 = arr2.indexOf(a2[0])
+        let id22 = arr2.indexOf(a2[a2.length - 1])
+
+        for (var i = 0; i < a1.length; i++) {
+            ts[a1[i][0]] = a1[i]
+        }
+
+        for (var i = 0; i < a2.length; i++) {
+            ts[a2[i][0]] = a2[i]
+        }
+
+        let ts_sorted = Object.keys(ts).sort()
+
+        return {
+            od: ts_sorted.map(x => ts[x]),
+            d1: [id11, id12 - id11 + 1],
+            d2: [id21, id22 - id21 + 1]
+        }
+
+    }
+
+    // Combine parts together:
+    // (destination, overlap, source)
+    combine(dst, o, src) {
+
+        function last(arr) { return arr[arr.length - 1][0] }
+
+        if (!dst.length) { dst = o; o = [] }
+        if (!src.length) { src = o; o = [] }
+
+        // The overlap right in the middle
+        if (src[0][0] >= dst[0][0] && last(src) <= last(dst)) {
+
+            return Object.assign(dst, o)
+
+        // The overlap is on the right
+        } else if (last(src) > last(dst)) {
+
+            // Psh(...) is faster but can overflow the stack
+            if (o.length < 100000 && src.length < 100000) {
+                dst.push(...o, ...src)
+                return dst
+            } else {
+                return dst.concat(o, src)
+            }
+
+        // The overlap is on the left
+        } else if (src[0][0] < dst[0][0]) {
+
+            // Push(...) is faster but can overflow the stack
+            if (o.length < 100000 && src.length < 100000) {
+                src.push(...o, ...dst)
+                return src
+            } else {
+                return src.concat(o, dst)
+            }
+
+        } else {  return []  }
+
+    }
+
 
 }
