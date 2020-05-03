62b133a2c4c9c4d995ae32e9e257a3a31f317e64
C451 <codeallright@protonmail.com>
2020-05-03 12:20:42 +0300

    Update build index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import TradingVue from './TradingVue.vue'
 import DataCube from './helpers/datacube.js'
 import Overlay from './mixins/overlay.js'
+import Tool from './mixins/tool.js'
+import Interface from './mixins/interface.js'
 import Utils from './stuff/utils.js'
 import Constants from './stuff/constants.js'
 import Candle from './components/primitives/candle.js'
 import Volbar from './components/primitives/volbar.js'
+import Line from './components/primitives/line.js'
+import Pin from './components/primitives/pin.js'
+import Price from './components/primitives/price.js'
+import Ray from './components/primitives/ray.js'
+import Seg from './components/primitives/seg.js'
+
 import { layout_cnv, layout_vol } from
     './components/js/layout_cnv.js'
 
+const primitives = {
+    Candle, Volbar, Line, Pin, Price, Ray, Seg
+}
 
 TradingVue.install = function (Vue) {
     Vue.component(TradingVue.name, TradingVue)
@@ -18,7 +29,7 @@ if (typeof window !== 'undefined' && window.Vue) {
     window.TradingVueLib = {
         TradingVue, Overlay, Utils, Constants,
         Candle, Volbar, layout_cnv, layout_vol,
-        DataCube
+        DataCube, Tool, Interface, primitives
     }
 }
 
@@ -27,5 +38,5 @@ export default TradingVue
 export {
     TradingVue, Overlay, Utils, Constants,
     Candle, Volbar, layout_cnv, layout_vol,
-    DataCube
+    DataCubem, Tool, Interface, primitives
 }
