73fc6d4cad6ff6aa8f01650edfdefccd099e1594
C451 <codeallright@protonmail.com>
2020-11-17 19:14:51 +0300

    LineTool: add Ray mode

--- src/components/primitives/ray.js
+++ src/components/primitives/ray.js
@@ -1,2 +1,11 @@
 
 // Draws a ray, adds corresponding collision f-n
+
+import Line from './line.js'
+
+export default class Ray extends Line {
+    constructor(overlay, ctx) {
+        super(overlay, ctx)
+        this.ray = true
+    }
+}
