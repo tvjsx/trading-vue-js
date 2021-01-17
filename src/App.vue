f3be79e7f44e4b8854b77dd1ceb5b78aebbd09d2
Oukaasha Habib <okasha.habib@gmail.com>
2021-01-17 20:19:49 +0500

    scroll to start or end of graph by pressing right or left arrow key. get the timestamp of candle by ctrl+click.

--- src/App.vue
+++ src/App.vue
@@ -20,14 +20,82 @@ export default {
         onResize() {
             this.width = window.innerWidth
             this.height = window.innerHeight
+        },
+        keydown(e) {
+            if (e.keyCode == 39) {
+                // ArrowRight
+                this.move(1)
+            } else if (e.keyCode == 37) {
+                // ArrowLeft
+                this.move(-1)
+            }
+        },
+        mousedown(e) {
+            var evtobj = window.event ? event : e
+            if (evtobj.ctrlKey) {
+                this.selectedCandleTimestamp = this.chart.tv.getCursor().t
+            }
+        },
+        move(direction) {
+            let ez = x => x<.5 ? 2*x*x : -1+(2*x)*x
+
+            if (direction > 0) {
+                let tN = this.chart.tv.$refs.chart.last_candle[0]
+                let t0 = this.chart.tv.getRange()[1]
+                let t1 = tN + this.chart.tv.$refs.chart.interval * 5
+    
+                let len = t1 - t0
+                let step = len / 1000
+                let t = t0
+    
+                let f = () => {
+                    if (t >= t1) return this.chart.tv.goto(t1)
+                    this.chart.tv.goto(t)
+                    t += step
+                    setTimeout(f)
+                }
+                f()
+            } else {
+                let tN = this.chart.data.chart.data[0][0]
+                let t0 = this.chart.tv.getRange()[1]
+                let t1 = tN + this.chart.tv.$refs.chart.interval * 50
+    
+                let len = t0 - t1
+                let step = len / 1000
+                let t = t0
+    
+                let f = () => {
+                    if (t <= t1) return this.chart.tv.goto(t1)
+                    this.chart.tv.goto(t)
+                    t -= step
+                    setTimeout(f)
+                }
+                f()
+            }
         }
     },
     mounted() {
         window.addEventListener('resize', this.onResize)
+        window.addEventListener('keydown', this.keydown)
+        window.addEventListener('mousedown', this.mousedown)
         window.dc = this.chart
+
+        /**
+         * The following code is for setting the initial range i.e. zoom level and the points which will be shown by default.
+         * startingTimestamp and endingTimestamp might come from an external source.
+         * I've added sample values for testing.
+         */
+        // let startingTimestamp = 1558015200000
+        // let endingTimestamp = 1558688400000
+        // Wait until chart is fully loaded
+        // this.$nextTick(() =>
+        //     this.chart.tv.$refs.chart.setRange(startingTimestamp, endingTimestamp)
+        // )
     },
     beforeDestroy() {
         window.removeEventListener('resize', this.onResize)
+        window.removeEventListener('keydown', this.keydown)
+        window.removeEventListener('mousedown', this.mousedown)
     },
     data() {
         return {
@@ -38,7 +106,8 @@ export default {
                 colorBack: '#fff',
                 colorGrid: '#eee',
                 colorText: '#333',
-            }
+            },
+            selectedCandleTimestamp: 0,
         };
     }
 };
